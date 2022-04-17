f75198a1315cae3acaded037f3cf6cdb1a777365
Vaibhavi <porevaibhavi5@gmail.com>
2022-04-17 21:59:53 +0530

    :hammer: Updated

--- BestFit.c
+++ BestFit.c
@@ -1,53 +1,52 @@
 #include<stdio.h>
-#define MAX 50
+#define MAX 20
+#define INF 99999
 
 void main()
 {
-    int fragment[MAX],blocks[MAX],processes[MAX],i,j,numBlocks,numProcesses,frag,lowest=9999,count=0,no_allocation_process=0;//fragment array = no. and size of the internal fragments created , b=no of blocks array , p=no. of processes array , nb = no. of blocks , np = no. of processes
-    int allocBlocks[MAX],allocProcesses[MAX];//barray=allocated block array , parray=allocated processes array , indicates the block no. allocated
-    printf("\nMemory Allocation Technique - Best Fit : \n");
+    int fragment[MAX],blocks[MAX],processes[MAX],i,j,numBlocks,numProc,frag,lowestFrag=INF;
+    int allocProc[MAX];
+    int ttlFrag=0;
+    printf("\n\tMemory Allocation Technique - Best Fit : \n");
     printf("\nEnter the number of blocks : ");
     scanf("%d",&numBlocks);
     printf("Enter the number of processes : ");
-    scanf("%d",&numProcesses);
+    scanf("%d",&numProc);
     printf("\nEnter the size of the blocks : \n");
     for(i=1;i<=numBlocks;i++)
     {
-        printf("Block no. %d : ",i);
-        scanf("%d",&allocBlocks[i]);
+        printf("Block-%d : ",i);
+        scanf("%d",&blocks[i]);
     }
     printf("\nEnter the size of the processes : \n");
-    for(i=1;i<=numProcesses;i++)
+    for(i=1;i<=numProc;i++)
     {
-        printf("Process no. %d : ",i);
-        scanf("%d",&allocProcesses[i]);
+        printf("Process-%d : ",i);
+        scanf("%d",&processes[i]);
     }
-    for(i=1;i<=numProcesses;i++)
+    for(i=1;i<=numProc;i++)
     {
+        lowestFrag = INF;
         for(j=1;j<=numBlocks;j++)
         {
-            if(allocBlocks[j]!=1)//checking if the block is allocated or free
-            {
-                frag=allocBlocks[j]-allocProcesses[i];//calculating internal fragmentation
-                if(frag>=0)
-                {
-                    if(lowest>frag)
-                    {
-                        allocProcesses[i]=j;
-                        lowest=frag;
-                    }
-                    
+            frag = blocks[j]-processes[i];
+            if(frag>=0){
+                if(frag<lowestFrag){
+                    lowestFrag = frag;
+                    allocProc[i] = j;
+                    fragment[i] = lowestFrag;
                 }
-
             }
         }
-        fragment[i]=lowest;
-        allocBlocks[allocProcesses[i]]=1;
-        lowest=10000;
+        if(lowestFrag == INF){printf("\nProcess-%d cant be allocated", i); allocProc[i]=-1;}
     }
-    printf("\nProcess no. \tProcess size\tBlock no. \tBlock size\tFragment\n");
-    for(i=1;i<=numProcesses && allocProcesses[i]!=0;i++)
+
+    printf("\nProcess \tProcess size\tBlock no. \tBlock size\tFragment\n");
+    for(i=1;i<=numProc && allocProc[i]!=-1;i++)
     {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",i,processes[i],allocProcesses[i],blocks[allocProcesses[i]],fragment[i]);
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",i,processes[i],allocProc[i],blocks[allocProc[i]],fragment[i]);
+        ttlFrag += fragment[i];
     }
+
+    printf("\nTotal Fragmentation = %d", ttlFrag);
 }
